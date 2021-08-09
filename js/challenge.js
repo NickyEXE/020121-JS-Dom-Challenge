79028451a2004f451c9ad560f090d9f3f548353a
NickyEXE <dovern42@gmail.com>
2021-08-09 18:52:54 -0400

    add like feature

--- js/challenge.js
+++ js/challenge.js
@@ -3,8 +3,15 @@ const dom = {
   plus: document.getElementById("plus"),
   minus: document.getElementById("minus"),
   pause: document.getElementById("pause"),
-
+  heart: document.getElementById("heart"),
+  getCount: () => parseInt(dom.counter.innerText),
+  likes: document.querySelector(".likes"),
 }
+
+// basic selectors
+// .class
+// #id
+// tags you just call directly: "h1"
 
 const state = {
   paused: false
@@ -12,7 +19,7 @@ const state = {
 
 function count(num){
   if (!state.paused){
-    dom.counter.innerText = parseInt(dom.counter.innerText) + num
+    dom.counter.innerText = dom.getCount() + num
   }
 }
 
@@ -30,7 +37,19 @@ function togglePause(){
   }
 }
 
+function like(){
+  const second = dom.getCount()
+  const li = document.querySelector(`li[data-second='${second}']`)
+  if (li){
+    const newLikeCount = parseInt(li.querySelector("span").innerText) + 1
+    li.innerHTML = `${second} has been liked <span>${newLikeCount}</span> times.`
+  } else {
+    dom.likes.innerHTML += `<li data-second=${second}>${second} has been liked <span>1</span> time.</li>`
+  }
+}
+
 setInterval(() => count(1), 1000)
 dom.plus.addEventListener("click", () => count(1))
 dom.minus.addEventListener("click", () => count(-1))
 dom.pause.addEventListener("click", togglePause)
+dom.heart.addEventListener("click", like)
